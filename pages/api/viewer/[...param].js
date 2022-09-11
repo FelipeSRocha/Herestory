e6cae8cf4bc4ab20e7ca9b3002b9cac58ceaab25
RochaFelpuda <felipe.21c@hotmail.com>
2022-09-11 12:24:03 -0300

    ♻️ Star to update to REST API

--- pages/api/viewer/[...param].js
+++ pages/api/viewer/[...param].js
@@ -8,7 +8,6 @@ const handler = async (req, res) => {
         const story_id = req.query.story_id
         switch (req.method) {
             case "GET":
-                console.log("get")
                 const story_single = await Model.findOne({
                     user,
                     story_id,
@@ -26,6 +25,28 @@ const handler = async (req, res) => {
             case "DELETE":
                 break;
         }
+    } else if (req.query.param[0] == "user" && req.query.user ){
+        const user = req.query.user
+        switch (req.method) {
+            case "GET":
+                console.log("get")
+                const story_list = await Model.find({
+                    user,
+                    published: true
+                });
+                res.status(200).json({story_list})
+
+                break;
+
+            case "POST":
+                break;
+
+            case "PUT":
+                break;
+
+            case "DELETE":
+                break;
+        } 
     }
 
 };
