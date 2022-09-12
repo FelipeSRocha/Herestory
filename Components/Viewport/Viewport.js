7cf74c5d7387f9bd8092814d43a53c515cb1860b
RochaFelpuda <felipe.21c@hotmail.com>
2022-09-12 18:53:00 -0300

    📱 Added event listener to resize

--- Components/Viewport/Viewport.js
+++ Components/Viewport/Viewport.js
@@ -2,12 +2,15 @@ import { useEffect } from "react";
 import * as S from "./style";
 
 const Viewport = ({ children }) => {
-    useEffect(()=>{
+    const resetHeight = () => {
         const view = document.getElementById("Viewport");
-        let vh = window.innerHeight
-        console.log(vh)
+        let vh = window.innerHeight;
         view.style.height = `${vh}px`;
-    },[])
+    };
+    useEffect(() => {
+        resetHeight;
+        window.addEventListener("resize", resetHeight);
+    }, []);
     return <S.Viewport id="Viewport">{children}</S.Viewport>;
 };
 export default Viewport;
